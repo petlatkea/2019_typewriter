e584e1a2faae09cb10280c35d9f123c313db26ee
Simona <simona.semancova@gmail.com>
2019-02-11 21:40:03 +0100

    typing

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,21 @@
 "use strict";
+let typewriter = document.getElementById("typewriter").innerHTML;
+let counter = 0;
+let speed = 250;
+
+document.getElementById("typewriter").innerHTML = "";
+
+function type() {
+  if (counter < typewriter.length) {
+    document.getElementById("typewriter").innerHTML += typewriter.charAt(
+      counter
+    );
+    counter++;
+    setTimeout(type, speed);
+  }
+}
+
+setTimeout(type, speed);
+//for (counter = 0; counter < typewriter.lenght; counter++) {}
+
+//document.getElementById("typewriter").textContent = "";
